2fa67dca54fa9a57ed19d1a4da4f3b67268d9605
CHAE <dbscogus4467@naver.com>
2023-11-16 18:04:51 +0900

    style: font style 상수화
    [#38]

---
+++ packages/frontend/src/styles/typography.ts
@@ -0,0 +1,24 @@
+const $semantic = {
+  h1: "mm-semantic-typography-h1",
+  h2: "mm-semantic-typography-h2",
+  h3: "mm-semantic-typography-h3",
+  title1Regular: "mm-semantic-typography-title1-regular",
+  title1Bold: "mm-semantic-typography-title1-bold",
+  title2Regular: "mm-semantic-typography-title2-regular",
+  title2Bold: "mm-semantic-typography-title2-bold",
+  title3Regular: "mm-semantic-typography-title3-regular",
+  title3Bold: "mm-semantic-typography-title3-bold",
+  title4Regular: "mm-semantic-typography-title4-regular",
+  title4Bold: "mm-semantic-typography-title4-bold",
+  body1Regular: "mm-semantic-typography-body1-regular",
+  body1Bold: "mm-semantic-typography-body1-bold",
+  body2Regular: "mm-semantic-typography-body2-regular",
+  body2Bold: "mm-semantic-typography-body2-bold",
+  caption1Regular: "mm-semantic-typography-caption1-regular",
+  caption1Bold: "mm-semantic-typography-caption1-bold",
+  caption2Regular: "mm-semantic-typography-caption2-regular",
+  caption2Bold: "mm-semantic-typography-caption2-bold",
+};
+
+const typography = { $semantic };
+export default typography;
