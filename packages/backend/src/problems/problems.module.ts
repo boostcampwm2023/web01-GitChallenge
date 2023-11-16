6f937af5a60ddc7b736d8c068b76ab1511a09daf
luizy <luizy991212@gmail.com>
2023-11-16 16:04:24 +0900

    feat: Problem 모듈 add
    Problem Controller Service 생성
    
    [#5]

---
+++ packages/backend/src/problems/problems.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { ProblemsController } from './problems.controller';
+import { ProblemsService } from './problems.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Problem } from './problem.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Problem])],
+  controllers: [ProblemsController],
+  providers: [ProblemsService],
+})
+export class ProblemsModule {}
---
+++ packages/backend/src/problems/problems.service.ts
@@ -0,0 +1,25 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Problem } from './problem.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class ProblemsService {
+  constructor(
+    @InjectRepository(Problem)
+    private problemRepository: Repository<Problem>,
+  ) {}
+
+  async getProblemById(id: number): Promise<Problem> {
+    const problem = await this.problemRepository.findOne({
+      where: { id },
+      relations: ['keywords', 'category'],
+    });
+
+    if (!problem) {
+      throw new NotFoundException(`Problem${id} not found`);
+    }
+
+    return problem;
+  }
+}
