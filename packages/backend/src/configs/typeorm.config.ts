63ba940aed3e6fb313555478e33597b069260379
luizy <luizy991212@gmail.com>
2023-11-16 16:01:30 +0900

    config: add typeORM
    yarn add -D typeORM
    
    [#5]

---
+++ packages/backend/src/configs/typeorm.config.ts
@@ -0,0 +1,8 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const typeOrmConfig: TypeOrmModuleOptions = {
+  type: 'sqlite',
+  database: 'db.sqlite',
+  entities: [__dirname + '/../**/*.entity.{js,ts}'],
+  synchronize: true,
+};
