13c2374f4ac1b14a903d5d12d0260449667086b7
YuHyun <puh0128@gmail.com>
2023-11-18 00:04:32 +0900

    config: React.StrictMode 시 react-toastify toast가 두 번 렌더링되는 문제
    [#40]

--- packages/frontend/next.config.js
+++ packages/frontend/next.config.js
@@ -1,12 +1,10 @@
-const {
-  createVanillaExtractPlugin
-} = require('@vanilla-extract/next-plugin');
+const { createVanillaExtractPlugin } = require("@vanilla-extract/next-plugin");
 
 const withVanillaExtract = createVanillaExtractPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+  reactStrictMode: false, // react-toastify toast 두 번 렌더링되는 문제
+};
 
 module.exports = withVanillaExtract(nextConfig);
--- packages/frontend/src/pages/_app.tsx
+++ packages/frontend/src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import "../styles/global.css";
+
 import type { AppProps } from "next/app";
+import React from "react";
 
 import { ToastContainer } from "../components/common";
 
@@ -8,7 +10,9 @@ import "react-toastify/dist/ReactToastify.min.css";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Component {...pageProps} />
+      <React.StrictMode>
+        <Component {...pageProps} />
+      </React.StrictMode>
       <ToastContainer />
     </>
   );
