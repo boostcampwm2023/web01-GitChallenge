47f2ddfb414e0ae2cb10a73905e24853cd0ae1d8
luizy <luizy991212@gmail.com>
2023-11-18 14:30:56 +0900

    refactor: entity 디렉터리 수정
    [#53]

--- packages/backend/src/problems/problems.module.ts
+++ packages/backend/src/problems/problems.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { ProblemsController } from './problems.controller';
 import { ProblemsService } from './problems.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Problem } from './problem.entity';
+import { Problem } from './entity/problem.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Problem])],
--- packages/backend/src/problems/problems.service.ts
+++ packages/backend/src/problems/problems.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Problem } from './problem.entity';
+import { Problem } from './entity/problem.entity';
 import { Repository } from 'typeorm';
 
 @Injectable()
