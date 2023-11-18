5356a11f092785eca7bc7f10d2d33228838f44d2
luizy <luizy991212@gmail.com>
2023-11-18 14:59:13 +0900

    feat: problem dta add
    [#53]

--- packages/backend/src/problems/problems.controller.ts
+++ packages/backend/src/problems/problems.controller.ts
@@ -1,13 +1,23 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { ProblemsService } from './problems.service';
-import { Problem } from './entity/problem.entity';
+import { ProblemDto } from './dto/problem.dto';
 
 @Controller('problems')
 export class ProblemsController {
   constructor(private readonly problemsService: ProblemsService) {}
 
   @Get(':id')
-  async getProblemById(@Param('id') id: number): Promise<Problem> {
-    return await this.problemsService.getProblemById(id);
+  async getProblemById(@Param('id') id: number): Promise<ProblemDto> {
+    const problem = await this.problemsService.getProblemById(id);
+
+    const problemDto: ProblemDto = {
+      id: problem.id,
+      title: problem.title,
+      description: problem.description,
+      keywords: problem.keywords.map((keyword) => keyword.keyword),
+      category: problem.category.name,
+    };
+
+    return problemDto;
   }
 }
