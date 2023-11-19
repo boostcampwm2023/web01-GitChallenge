19f0fd49c991de58abeaa4a54904319a3b69c4ed
YuHyun <puh0128@gmail.com>
2023-11-19 17:10:23 +0900

    refactor: Toast > index 파일 추가
    [#40]

--- packages/frontend/src/components/common/index.ts
+++ packages/frontend/src/components/common/index.ts
@@ -1,4 +1,3 @@
 export { Button } from "./Button";
 export { Badge, BadgeGroup } from "./Badge";
-export { default as toast } from "./Toast/toast";
-export { default as ToastContainer } from "./Toast/ToastContainer";
+export { toast, ToastContainer } from "./Toast";
