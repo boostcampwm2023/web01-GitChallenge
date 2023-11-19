0ef9e13ea55362c724a8d2a41eca32053eeea9c1
CHAE <dbscogus4467@naver.com>
2023-11-19 12:17:47 +0900

    feat: objectKeys 타입 유틸 추가
    [#42]

---
+++ packages/frontend/src/utils/types.ts
@@ -0,0 +1,10 @@
+export type ObjectKeys<T extends Record<PropertyKey, unknown>> = `${Exclude<
+  keyof T,
+  symbol
+>}`;
+
+export function objectKeys<Type extends Record<PropertyKey, unknown>>(
+  obj: Type,
+): Array<ObjectKeys<Type>> {
+  return Object.keys(obj) as Array<ObjectKeys<Type>>;
+}
