0f26acdebd74190a9fea26320b721dcdf7e733e4
CHAE <dbscogus4467@naver.com>
2023-11-19 22:40:04 +0900

    chore: 테스트용 background-color 삭제
    [#64]

--- packages/frontend/src/design-system/tokens/layout.css.ts
+++ packages/frontend/src/design-system/tokens/layout.css.ts
@@ -1,6 +1,5 @@
 import { style } from "@vanilla-extract/css";
 
-import color from "./color";
 import * as utils from "./utils.css";
 
 export const header = style([
@@ -11,7 +10,6 @@ export const header = style([
     position: "fixed",
     top: 0,
     left: 0,
-    backgroundColor: color.$scale.grey700,
   },
 ]);
 
@@ -19,18 +17,16 @@ export const base = style([
   utils.flex,
   {
     width: 1440,
+    height: 650,
     marginTop: 56,
-    backgroundColor: color.$scale.grey900,
   },
 ]);
 
 export const sideBar = style({
   width: 213,
-  backgroundColor: color.$scale.grey300,
 });
 
 export const container = style({
   width: 1226,
-  backgroundColor: color.$scale.grey500,
   margin: "0 60px",
 });
