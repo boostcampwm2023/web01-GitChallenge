dea8ac12689502ad01b1b4520c4a8080c1af48ab
luizy <luizy991212@gmail.com>
2023-11-20 13:31:46 +0900

    feat: /quizzes get api add
    - controller edit
    - module categoryEntity uses add
    - service edit
    - quizzes.dto add
    
    [#78]

--- packages/backend/src/quizzes/quizzes.module.ts
+++ packages/backend/src/quizzes/quizzes.module.ts
@@ -3,9 +3,10 @@ import { QuizzesController } from './quizzes.controller';
 import { QuizzesService } from './quizzes.service';
 import { Quiz } from './entity/quiz.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Category } from './entity/category.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Quiz])],
+  imports: [TypeOrmModule.forFeature([Quiz, Category])],
   controllers: [QuizzesController],
   providers: [QuizzesService],
 })
--- packages/backend/src/quizzes/quizzes.service.ts
+++ packages/backend/src/quizzes/quizzes.service.ts
@@ -3,12 +3,16 @@ import { Quiz } from './entity/quiz.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { QuizDto } from './dto/quiz.dto';
+import { CategoryQuizzesDto, QuizzesDto } from './dto/quizzes.dto';
+import { Category } from './entity/category.entity';
 
 @Injectable()
 export class QuizzesService {
   constructor(
     @InjectRepository(Quiz)
     private quizRepository: Repository<Quiz>,
+    @InjectRepository(Category)
+    private categoryRepository: Repository<Category>,
   ) {}
   async getQuizById(id: number): Promise<QuizDto> {
     const quiz = await this.quizRepository.findOne({
@@ -29,5 +33,26 @@ export class QuizzesService {
     };
 
     return quizDto;
+  }
+
+  async findAllProblemsGroupedByCategory(): Promise<QuizzesDto> {
+    const categories = await this.categoryRepository.find({
+      relations: ['quizzes'],
+    });
+
+    const categoryQuizzesDtos: CategoryQuizzesDto[] = categories.map(
+      (category) => ({
+        id: category.id,
+        category: category.name,
+        problems: category.quizzes.map((quiz) => ({
+          id: quiz.id,
+          title: quiz.title,
+        })),
+      }),
+    );
+
+    const quizzesDtos: QuizzesDto = { categories: categoryQuizzesDtos };
+
+    return quizzesDtos;
   }
 }
