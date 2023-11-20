0b376afc57fd579d882ae6254c6ddc940b848066
CHAE <dbscogus4467@naver.com>
2023-11-20 21:49:31 +0900

    feat: 헤더 구현
    [#62]

--- packages/frontend/src/design-system/components/common/index.ts
+++ packages/frontend/src/design-system/components/common/index.ts
@@ -1,3 +1,4 @@
+export { default as Header } from "./Header";
 export { default as Button } from "./Button";
 export { Badge, BadgeGroup } from "./Badge";
 export { toast, ToastContainer } from "./Toast";
