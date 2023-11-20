8996c45cc7b224259746231f7382bc58452631c3
CHAE <dbscogus4467@naver.com>
2023-11-20 17:46:04 +0900

    feat: React Portal용 modal 요소 선언
    [#41]

--- packages/frontend/src/pages/_document.tsx
+++ packages/frontend/src/pages/_document.tsx
@@ -1,10 +1,11 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html data-theme="light" lang="en">
       <Head />
       <body>
+        <div id="modal" />
         <Main />
         <NextScript />
       </body>
