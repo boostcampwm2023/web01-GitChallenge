64ae9738f53795f749c7c490e91eb96d61b685d4
luizy <luizy991212@gmail.com>
2023-11-20 13:35:10 +0900

    refactor: problem module delete
    [#78]

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProblemsModule } from './problems/problems.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './configs/typeorm.config';
 import { QuizzesModule } from './quizzes/quizzes.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), ProblemsModule, QuizzesModule],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), QuizzesModule],
   controllers: [AppController],
   providers: [AppService],
 })
