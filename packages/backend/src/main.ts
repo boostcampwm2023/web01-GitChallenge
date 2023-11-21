e7eed3b8198b783a1f9c6241709dcf5643ba380c
flydog98 <guinnessoverflow@gmail.com>
2023-11-22 00:53:56 +0900

    feat: main.ts에 swagger bootstrap 추가
    - quizzes라는 api 태그를 만들고 버전이 1.0임을 명시합니다.
    - Title과 Description을 작성했습니다.
    
    [#59]

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,10 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.use(cookieParser());
+
+  const config = new DocumentBuilder()
+    .setTitle("Merge Masters' Git Challenge API")
+    .setDescription('Git Challenge의 API 설명서입니다! 파이팅!')
+    .setVersion('1.0')
+    .addTag('quizzes')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(8080);
 }
 bootstrap();
