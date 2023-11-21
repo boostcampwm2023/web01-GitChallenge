090d6a805d120c2baba9022d0357337872c750e5
CHAE <dbscogus4467@naver.com>
2023-11-21 19:07:11 +0900

    feat: _app.tsx 파일에 Layout 컴포넌트 적용
    [#67]

--- packages/frontend/src/pages/_app.tsx
+++ packages/frontend/src/pages/_app.tsx
@@ -6,13 +6,16 @@ import React from "react";
 import "react-toastify/dist/ReactToastify.min.css";
 
 import { ToastContainer } from "../design-system/components/common";
+import Layout from "../design-system/components/common/Layout";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <React.StrictMode>
-        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-        <Component {...pageProps} />
+        <Layout>
+          {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+          <Component {...pageProps} />
+        </Layout>
       </React.StrictMode>
       <ToastContainer />
     </>
