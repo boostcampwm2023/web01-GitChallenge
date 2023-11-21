95a2cadc6c80f2cb7fac320690c2a87742ca3634
luizy <luizy991212@gmail.com>
2023-11-21 18:41:44 +0900

    feat: sessionService feature add
    - createSession
    - getContainerIdBySessionId
    - setContainerBySessionId
    - getSessionById
    
    [#70]

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -5,7 +5,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { typeOrmConfig } from './configs/typeorm.config';
 import { QuizzesModule } from './quizzes/quizzes.module';
-import { SessionModule } from './session/session.module';
 
 @Module({
   imports: [
@@ -14,7 +13,6 @@ import { SessionModule } from './session/session.module';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    SessionModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- packages/backend/src/session/session.service.ts
+++ packages/backend/src/session/session.service.ts
@@ -1,4 +1,63 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Session } from './schema/session.schema';
+import { ObjectId } from 'typeorm';
 
 @Injectable()
-export class SessionService {}
+export class SessionService {
+  constructor(@InjectModel(Session.name) private sessionModel: Model<Session>) {
+    const testSession = new this.sessionModel({
+      problems: {},
+    });
+    testSession.save();
+  }
+
+  async createSession(): Promise<string> {
+    const session = new this.sessionModel({
+      problems: {},
+    });
+    return await session.save().then((session) => {
+      console.log('session created');
+      return (session._id as ObjectId).toString('hex');
+    });
+  }
+
+  async getContainerIdBySessionId(
+    sessionId: string,
+    problemId: number,
+  ): Promise<string> {
+    const session = await this.getSessionById(sessionId);
+
+    if (!session.problems[problemId]) {
+      session.problems.set(problemId.toString(), {
+        status: 'solving',
+        logs: [],
+        containerId: '',
+      });
+      console.log('session updated');
+      console.log(session.problems[problemId]);
+      await session.save();
+    }
+    return session.problems[problemId]?.containerId;
+  }
+
+  async setContainerBySessionId(
+    sessionId: string,
+    problemId: number,
+    containerId: string,
+  ): Promise<void> {
+    const session = await this.getSessionById(sessionId);
+    console.log(session);
+    console.log(problemId);
+    if (!session.problems.get(problemId.toString())) {
+      throw new Error('problem not found');
+    }
+    session.problems.get(problemId.toString()).containerId = containerId;
+    session.save();
+  }
+
+  private async getSessionById(id: string): Promise<Session> {
+    return await this.sessionModel.findById(id);
+  }
+}
