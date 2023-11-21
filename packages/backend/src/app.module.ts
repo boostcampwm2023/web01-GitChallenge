39dc93bbb4b9f205d24fd704795ff85301655cbd
luizy <luizy991212@gmail.com>
2023-11-21 14:39:20 +0900

    feat: session module, service add
    [#70]

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -5,6 +5,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { typeOrmConfig } from './configs/typeorm.config';
 import { QuizzesModule } from './quizzes/quizzes.module';
+import { SessionModule } from './session/session.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { QuizzesModule } from './quizzes/quizzes.module';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    SessionModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- packages/backend/src/quizzes/quizzes.module.ts
+++ packages/backend/src/quizzes/quizzes.module.ts
@@ -5,9 +5,14 @@ import { Quiz } from './entity/quiz.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Category } from './entity/category.entity';
 import { ContainersModule } from '../containers/containers.module';
+import { SessionModule } from '../session/session.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Quiz, Category]), ContainersModule],
+  imports: [
+    TypeOrmModule.forFeature([Quiz, Category]),
+    ContainersModule,
+    SessionModule,
+  ],
   controllers: [QuizzesController],
   providers: [QuizzesService],
 })
