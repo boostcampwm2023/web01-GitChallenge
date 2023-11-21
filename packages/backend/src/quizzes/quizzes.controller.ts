c3859ae191be5f9bc62d218b55915015bc71dc8c
luizy <luizy991212@gmail.com>
2023-11-21 18:41:44 +0900

    feat: sessionService feature add
    - createSession
    - getContainerIdBySessionId
    - setContainerBySessionId
    - getSessionById
    
    [#70]

--- packages/backend/src/quizzes/quizzes.controller.ts
+++ packages/backend/src/quizzes/quizzes.controller.ts
@@ -6,16 +6,25 @@ import {
   Body,
   HttpException,
   HttpStatus,
+  Res,
+  Req,
 } from '@nestjs/common';
 import { QuizDto } from './dto/quiz.dto';
 import { QuizzesService } from './quizzes.service';
 import { QuizzesDto } from './dto/quizzes.dto';
 import { CommandRequestDto } from './dto/command-request.dto';
 import { CommandResponseDto } from './dto/command-response.dto';
+import { SessionService } from '../session/session.service';
+import { Request, Response } from 'express';
+import { ContainersService } from '../containers/containers.service';
 
 @Controller('api/v1/quizzes')
 export class QuizzesController {
-  constructor(private readonly quizService: QuizzesService) {}
+  constructor(
+    private readonly quizService: QuizzesService,
+    private readonly sessionService: SessionService,
+    private readonly containerService: ContainersService,
+  ) {}
 
   @Get(':id')
   async getProblemById(@Param('id') id: number): Promise<QuizDto> {
@@ -33,11 +42,46 @@ export class QuizzesController {
   async runGitCommand(
     @Param('id') id: number,
     @Body() execCommandDto: CommandRequestDto,
+    @Res() response: Response,
+    @Req() request: Request,
   ): Promise<CommandResponseDto> {
+    let sessionId = request.cookies?.sessionId;
+
+    if (!sessionId) {
+      // 세션 아이디가 없다면
+      response.cookie(
+        'sessionId',
+        (sessionId = await this.sessionService.createSession()),
+        {
+          httpOnly: true,
+          // 개발 이후 활성화 시켜야함
+          // secure: true,
+        },
+      ); // 세션 아이디를 생성한다.
+    }
+
+    // 살려야함
+    // let containerId = await this.sessionService.getContainerIdBySessionId(
+    //   sessionId,
+    //   id,
+    // );
+
+    // 살려야함
+    // if (!this.containerService.isValidateContainerId(containerId)) {
+    //   containerId = await this.containerService.getContainer(id);
+    //   await this.sessionService.setContainerBySessionId(
+    //     sessionId,
+    //     id,
+    //     containerId,
+    //   );
+    // }
+
     try {
-      const { message, result } = await this.quizService.runGitCommand(
+      const { message, result } = await this.containerService.runGitCommand(
+        'testContainer',
         execCommandDto.command,
       );
+
       return {
         message,
         result,
