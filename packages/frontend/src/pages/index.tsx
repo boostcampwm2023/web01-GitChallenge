ffd9b4cad5834130f8dc0ef88cb78506c0906772
CHAE <dbscogus4467@naver.com>
2023-11-21 16:07:35 +0900

    fix: 기존 코드 lint error 해결
    [#77]

--- packages/frontend/src/pages/index.tsx
+++ packages/frontend/src/pages/index.tsx
@@ -1,13 +1,1 @@
-import { Inter } from "next/font/google";
-import Head from "next/head";
-import Image from "next/image";
-
-import { Header } from "../design-system/components/common";
-
-import styles from "@/styles/Home.module.css";
-
-const inter = Inter({ subsets: ["latin"] });
-
-export default function Home() {
-  return <Header />;
-}
+export default function Home() {}
