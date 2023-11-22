8cc8b4a69ade434950ff0f5b6404c3fd51a2fb87
YuHyun <puh0128@gmail.com>
2023-11-23 03:12:54 +0900

    config: Storybook vanilla-extract, 테마 툴 바 설정
    [#105]

--- packages/frontend/.storybook/preview.ts
+++ packages/frontend/.storybook/preview.ts
@@ -1,5 +1,10 @@
 import type { Preview } from "@storybook/react";
 
+import { withThemeByDataAttribute } from "@storybook/addon-styling";
+
+import "../src/design-system/styles/global.css";
+import "../src/design-system/styles/reset.css";
+
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
@@ -10,6 +15,30 @@ const preview: Preview = {
       },
     },
   },
+  // https://storybook.js.org/docs/react/essentials/toolbars-and-globals#global-types-and-the-toolbar-annotation
+  globalTypes: {
+    theme: {
+      defaultValue: "light",
+      toolbar: {
+        title: "Theme",
+        icon: "circlehollow",
+        items: ["light", "dark"],
+        dynamicTitle: true,
+      },
+    },
+  },
 };
+
+// Not-using-React-or-JSX?-No-problem! https://storybook.js.org/blog/styling-addon-configure-styles-and-themes-in-storybook
+export const decorators = [
+  withThemeByDataAttribute({
+    themes: {
+      light: "light",
+      dark: "dark",
+    },
+    defaultTheme: "light",
+    attributeName: "data-theme",
+  }),
+];
 
 export default preview;
