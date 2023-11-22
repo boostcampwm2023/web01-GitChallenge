93cf05a0decc0f9f03c2f5f69c01337324c8cf8b
YuHyun <puh0128@gmail.com>
2023-11-22 20:00:46 +0900

    chore: 퀴즈 목데이터 추가
    [#3]

---
+++ packages/frontend/src/mocks/apis/data/quizContentData.ts
@@ -0,0 +1,27 @@
+const quizContentMockData = {
+  id: 3,
+  title: "git add & git status",
+  description: `당신은 새로운 팀에 배정되었습니다. 이제부터 전임자의 일을 이어서 진행해야 합니다. 
+배정된 팀의 저장소 전략은 다음과 같습니다. 
+
+1. 원본 저장소 \`upstream\`이 있다. 
+2. 우리 팀에서 \`upstream\`으로부터 fork한 저장소인 \`origin\`이 있다. 
+3. \`origin\`에서 branch를 생성하여 작업한다. 
+4. 당신은 \`origin\`으로 push할 수 있으며, 필요에 따라 \`upstream\`으로 PR을 생성할 수 있다. 
+
+\`origin\` 저장소 github 주소: https://github.com/flydog98/git-challenge-remote-upstream.git
+
+당신은 새로운 팀에 배정되었습니다. 이제부터 전임자의 일을 이어서 진행해야 합니다. 
+배정된 팀의 저장소 전략은 다음과 같습니다. 
+
+1. 원본 저장소 \`upstream\`이 있다. 
+2. 우리 팀에서 \`upstream\`으로부터 fork한 저장소인 \`origin\`이 있다. 
+3. \`origin\`에서 branch를 생성하여 작업한다. 
+4. 당신은 \`origin\`으로 push할 수 있으며, 필요에 따라 \`upstream\`으로 PR을 생성할 수 있다. 
+
+\`origin\` 저장소 github 주소: https://github.com/flydog98/git-challenge-remote-upstream.git`,
+  keywords: ["add", "status"],
+  category: "Git Start",
+};
+
+export default quizContentMockData;
