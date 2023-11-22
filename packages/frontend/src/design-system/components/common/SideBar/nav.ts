408ac0c6739cb778802aa00dc5a1f34537dc4767
CHAE <dbscogus4467@naver.com>
2023-11-22 12:16:54 +0900

    feat: SideBar 컴포넌트 구현
    [#67]

---
+++ packages/frontend/src/design-system/components/common/SideBar/nav.ts
@@ -0,0 +1,73 @@
+export const nav = [
+  {
+    title: "$ git help",
+    subTitles: [
+      {
+        subTitle: "명령어 이해하기",
+        href: "/",
+      },
+    ],
+  },
+  {
+    title: "git start",
+    subTitles: [
+      {
+        subTitle: "git 시작하기",
+        href: "/",
+      },
+      {
+        subTitle: "내 정보 설정하기",
+        href: "/",
+      },
+      {
+        subTitle: "파일 스테이징 하기",
+        href: "/",
+      },
+      {
+        subTitle: "커밋하기",
+        href: "/",
+      },
+      {
+        subTitle: "브랜치 만들기",
+        href: "/",
+      },
+      {
+        subTitle: "브랜치 바꾸기",
+        href: "/",
+      },
+    ],
+  },
+  {
+    title: "git advanced",
+    subTitles: [
+      {
+        subTitle: "커밋 메시지 수정하기",
+        href: "/",
+      },
+      {
+        subTitle: "커밋 되돌리기",
+        href: "/",
+      },
+      {
+        subTitle: "변경 사항 지우기",
+        href: "/",
+      },
+      {
+        subTitle: "변경 사항 저장하기",
+        href: "/",
+      },
+      {
+        subTitle: "커밋 가져오기",
+        href: "/",
+      },
+      {
+        subTitle: "커밋 이력 조작하기",
+        href: "/",
+      },
+      {
+        subTitle: "변경 사항 되돌리기",
+        href: "/",
+      },
+    ],
+  },
+] as const;
