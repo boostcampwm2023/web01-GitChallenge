ce0cadcab93178cd92deb25abc7d4052b50cf16d
CHAE <dbscogus4467@naver.com>
2023-11-23 17:51:40 +0900

    config: baseURL에 null 병합 연산자 적용

--- packages/frontend/src/apis/base.ts
+++ packages/frontend/src/apis/base.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const apiVersion = "/api/v1";
-const baseURL = `${process.env.NEXT_PUBLIC_API_HOST}${apiVersion}`;
+
+const baseURL = `${process.env.NEXT_PUBLIC_API_HOST ?? ""}${apiVersion}`;
 
 const instance = axios.create({
   baseURL,
