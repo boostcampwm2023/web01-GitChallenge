1f64c5266b7b83f1c13e772b16dba22b9071c036
luizy <luizy991212@gmail.com>
2023-11-23 19:05:17 +0900

    feat: init 쿼리 추가
    - quizzes service 첫 동작시 데이터 삽입
    - info level 로그 추가
    
    [#115]

--- packages/backend/src/quizzes/quizzes.service.ts
+++ packages/backend/src/quizzes/quizzes.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
 import { Quiz } from './entity/quiz.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
@@ -7,6 +7,10 @@ import { CategoryQuizzesDto, QuizzesDto } from './dto/quizzes.dto';
 import { Category } from './entity/category.entity';
 import { ContainersService } from 'src/containers/containers.service';
 import { CommandResponseDto } from './dto/command-response.dto';
+import fs from 'fs';
+import * as Papa from 'papaparse';
+import { Keyword } from './entity/keyword.entity';
+import { Logger } from 'winston';
 
 @Injectable()
 export class QuizzesService {
@@ -15,8 +19,81 @@ export class QuizzesService {
     private quizRepository: Repository<Quiz>,
     @InjectRepository(Category)
     private categoryRepository: Repository<Category>,
+    @InjectRepository(Keyword)
+    private keywordRepository: Repository<Keyword>,
     private containerService: ContainersService,
-  ) {}
+    @Inject('winston') private readonly logger: Logger,
+  ) {
+    this.initQiuzzes();
+  }
+
+  private async initQiuzzes() {
+    this.logger.log('info', `read git-challenge-quiz.csv`);
+    const quizData = await this.readCsvFile('git-challenge-quiz.csv');
+
+    // Category와 Keyword를 먼저 생성
+    const categories = {};
+    const keywords = {};
+
+    for (const data of quizData) {
+      // Category 처리
+      if (!categories[data.category]) {
+        let category = new Category();
+        category.name = data.category;
+        this.logger.log('info', `add ${category.name} to categories`);
+        category = await this.categoryRepository.save(category);
+        categories[data.category] = category;
+      }
+
+      // Keyword 처리
+      const keywordList = data.keyword.split(',');
+      for (const kw of keywordList) {
+        const trimmedKw = kw.trim();
+        if (!keywords[trimmedKw]) {
+          let keyword = new Keyword();
+          keyword.keyword = trimmedKw;
+          this.logger.log('info', `add ${keyword.keyword} to keywords`);
+          keyword = await this.keywordRepository.save(keyword);
+          keywords[trimmedKw] = keyword;
+        }
+      }
+    }
+
+    // Quiz 생성
+    for (const data of quizData) {
+      const quiz = new Quiz();
+      quiz.title = data.title;
+      quiz.description = data.description;
+      quiz.category = categories[data.category];
+      quiz.id = data.id;
+
+      const keywordList = data.keyword.split(',').map((kw) => kw.trim());
+      quiz.keywords = keywordList.map((kw) => keywords[kw]);
+
+      this.logger.log('info', `add ${quiz.title} to quizzes`);
+      await this.quizRepository.save(quiz);
+    }
+  }
+
+  private readCsvFile(filePath: string): Promise<any[]> {
+    return new Promise((resolve, reject) => {
+      const file = fs.createReadStream(filePath);
+      const results = [];
+
+      Papa.parse(file, {
+        header: true,
+        step: (row) => {
+          results.push(row.data);
+        },
+        complete: () => {
+          resolve(results);
+        },
+        error: (err) => {
+          reject(err);
+        },
+      });
+    });
+  }
   async getQuizById(id: number): Promise<QuizDto> {
     const quiz = await this.quizRepository.findOne({
       where: { id },
