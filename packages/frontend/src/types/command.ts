dc03db2ff64933c6741a4b15c34d734e1f5ad35f
CHAE <dbscogus4467@naver.com>
2023-11-23 17:04:16 +0900

    refactor: Command type에 vi 추가
    [#74]

--- packages/frontend/src/types/command.ts
+++ packages/frontend/src/types/command.ts
@@ -1,8 +1,9 @@
-export type CommandSuccess = "sucess";
+export type CommandSuccess = "success";
 export type CommandFail = "fail";
+export type CommandVI = "vi";
 
 export type Command = {
   message: string;
-  result: CommandSuccess | CommandFail;
+  result: CommandSuccess | CommandFail | CommandVI;
   graph?: string;
 };
