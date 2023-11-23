5cec71a400dda9eb031cf9139e3f144702ed331d
CHAE <dbscogus4467@naver.com>
2023-11-23 11:55:08 +0900

    feat: requestCommand API 구현
    [#74]

---
+++ packages/frontend/src/apis/request/quizzes.ts
@@ -0,0 +1,18 @@
+import { API_PATH } from "../../constants/path";
+import { Command } from "../../types/command";
+import { instance } from "../index";
+
+export const requestCommand = async ({
+  id,
+  command,
+}: {
+  id: number;
+  command: string;
+}) => {
+  const { data } = await instance.post<Command>(
+    `${API_PATH.QUIZZES}/${id}/command`,
+    { command },
+  );
+
+  return data;
+};
