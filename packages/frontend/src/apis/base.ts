c6d616b744eea870282c8ec4634c263d793374d9
CHAE <dbscogus4467@naver.com>
2023-11-23 21:11:28 +0900

    config: baseURL에 API_HOST 삭제

--- packages/frontend/src/apis/base.ts
+++ packages/frontend/src/apis/base.ts
@@ -2,10 +2,8 @@ import axios from "axios";
 
 const apiVersion = "/api/v1";
 
-const baseURL = `${process.env.NEXT_PUBLIC_API_HOST ?? ""}${apiVersion}`;
-
 const instance = axios.create({
-  baseURL,
+  baseURL: apiVersion,
 });
 
-export { baseURL, instance };
+export { instance };
