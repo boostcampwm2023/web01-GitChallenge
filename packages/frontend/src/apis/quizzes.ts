e442b50c8c409d68dc56cffebbd273515bb6bbf6
CHAE <dbscogus4467@naver.com>
2023-11-23 21:56:57 +0900

    feat: getQuiz API 구현
    [#118]

--- packages/frontend/src/apis/quizzes.ts
+++ packages/frontend/src/apis/quizzes.ts
@@ -1,19 +1,23 @@
 import { API_PATH } from "../constants/path";
-import { Command } from "../types/command";
+import { Command, Quiz } from "../types/quiz";
 
 import { instance } from "./base";
 
-export const requestCommand = async ({
-  id,
-  command,
-}: {
+export const quizAPI = {
+  postCommand: async ({ id, command }: PostCommandRequest) => {
+    const { data } = await instance.post<Command>(
+      `${API_PATH.QUIZZES}/${id}/command`,
+      { command },
+    );
+    return data;
+  },
+  getQuiz: async (id: string) => {
+    const { data } = await instance.get<Quiz>(`${API_PATH.QUIZZES}/${id}`);
+    return data;
+  },
+};
+
+type PostCommandRequest = {
   id: number;
   command: string;
-}) => {
-  const { data } = await instance.post<Command>(
-    `${API_PATH.QUIZZES}/${id}/command`,
-    { command },
-  );
-
-  return data;
 };
