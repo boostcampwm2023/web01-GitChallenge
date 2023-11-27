2182583716c99799b23c958e3fbcd100cc4b5d0d
flydog98 <guinnessoverflow@gmail.com>
2023-11-27 21:18:16 +0900

    feat: requestDTO 명세에 맞추어 변경 및 swagger 업데이트
    [#72]

--- packages/backend/src/quizzes/dto/command-request.dto.ts
+++ packages/backend/src/quizzes/dto/command-request.dto.ts
@@ -1,6 +1,17 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CommandRequestDto {
-  @ApiProperty({ description: '실행할 명령문', example: 'git branch' })
-  command: string;
+  @ApiProperty({
+    description:
+      '실행할 명령 모드. 예: "command" (명령 실행), "editor" (에디터 명령)',
+    example: 'command',
+  })
+  mode: string;
+
+  @ApiProperty({
+    description:
+      '실행할 명령문 or 에디터 작성 본문. 예: "git status (명령 실행), "feat: tmp.js 파일 제거" (에디터 명령)',
+    example: 'git status',
+  })
+  message: string;
 }
