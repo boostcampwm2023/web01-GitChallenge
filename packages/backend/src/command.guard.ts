0632088b54a4e8756bbf99c1d9878ec7f6830032
flydog98 <guinnessoverflow@gmail.com>
2023-11-28 00:14:41 +0900

    feat: 새로운 요구사항에 맞추어 Guard 로직 수정
    [#72]

--- packages/backend/src/command.guard.ts
+++ packages/backend/src/command.guard.ts
@@ -4,7 +4,12 @@ import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 export class CommandGuard implements CanActivate {
   canActivate(context: ExecutionContext): boolean {
     const request = context.switchToHttp().getRequest<Request>();
-    const command = request.body['command'];
-    return typeof command === 'string' && command.startsWith('git');
+    const mode = request.body['mode'];
+    const message = request.body['message'];
+    return (
+      typeof mode === 'string' &&
+      typeof message === 'string' &&
+      (mode === 'editor' || (mode === 'command' && message.startsWith('git')))
+    );
   }
 }
