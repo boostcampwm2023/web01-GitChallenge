a04bc38e6a3673ad3b7c49a0316cae847870340e
flydog98 <guinnessoverflow@gmail.com>
2023-11-29 00:41:41 +0900

    test: 2번 문제 채점 테스트 코드 작성
    [#145]

--- packages/backend/src/quiz-wizard/quiz-wizard.service.spec.ts
+++ packages/backend/src/quiz-wizard/quiz-wizard.service.spec.ts
@@ -4,6 +4,8 @@ import { ContainersService } from '../containers/containers.service';
 import { executeSSHCommand } from '../common/ssh.util';
 import { ConfigService } from '@nestjs/config';
 
+const DOCKER_TEMPLATE = 'docker exec -w /home/quizzer/quiz -u quizzer';
+
 const mockConfigService = {
   get: jest.fn((key) => {
     if (key === 'CONTAINER_GIT_USERNAME') {
@@ -38,7 +40,7 @@ describe('QuizWizardService', () => {
     containerService = module.get<ContainersService>(ContainersService);
   });
 
-  describe('1번 문제 테스트 코드', () => {
+  describe('1번 문제 채점 테스트 코드', () => {
     let containerId: string;
 
     beforeEach(async () => {
@@ -50,15 +52,38 @@ describe('QuizWizardService', () => {
     });
 
     it('init 이후 정답 받는 경우', async () => {
-      await executeSSHCommand(
-        `docker exec -w /home/quizzer/quiz -u quizzer ${containerId} git init`,
-      );
+      await executeSSHCommand(`${DOCKER_TEMPLATE} ${containerId} git init`);
 
       expect(await service.submit(containerId, 1)).toBe(true);
     });
 
     it('init 하지 않은 경우', async () => {
       expect(await service.submit(containerId, 1)).toBe(false);
+    });
+  });
+
+  describe('2번 문제 채점 테스트 코드', () => {
+    let containerId: string;
+
+    beforeEach(async () => {
+      containerId = await containerService.getContainer(2);
+    });
+
+    afterEach(async () => {
+      await executeSSHCommand(`docker rm -f ${containerId}`);
+    });
+
+    it('베스트 정답 케이스', async () => {
+      await executeSSHCommand(
+        `${DOCKER_TEMPLATE} ${containerId} git config user.name "Username"`,
+        `${DOCKER_TEMPLATE} ${containerId} git config user.email "useremail@email.com"`,
+      );
+
+      expect(await service.submit(containerId, 2)).toBe(true);
+    });
+
+    it('아무 동작 없는 케이스', async () => {
+      expect(await service.submit(containerId, 2)).toBe(false);
     });
   });
 });
