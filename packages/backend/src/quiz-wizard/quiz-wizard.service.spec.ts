cd165b29f77b93bb549d2d0c147d9cc90310fe2c
flydog98 <guinnessoverflow@gmail.com>
2023-11-28 23:53:54 +0900

    test: containerService를 위한 모킹 및 1번 문제 채점 테스트 코드 작성
    [#145]

--- packages/backend/src/quiz-wizard/quiz-wizard.service.spec.ts
+++ packages/backend/src/quiz-wizard/quiz-wizard.service.spec.ts
@@ -1,18 +1,64 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { QuizWizardService } from './quiz-wizard.service';
+import { ContainersService } from '../containers/containers.service';
+import { executeSSHCommand } from '../common/ssh.util';
+import { ConfigService } from '@nestjs/config';
+
+const mockConfigService = {
+  get: jest.fn((key) => {
+    if (key === 'CONTAINER_GIT_USERNAME') {
+      return 'quizzer';
+    }
+  }),
+};
+const mockLogger = {};
 
 describe('QuizWizardService', () => {
   let service: QuizWizardService;
+  let containerService: ContainersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [QuizWizardService],
+      providers: [
+        QuizWizardService,
+        ContainersService,
+        ContainersService,
+        {
+          provide: ConfigService,
+          useValue: mockConfigService, // 여기서 mockConfigService는 모킹된 ConfigService
+        },
+        {
+          provide: 'winston',
+          useValue: mockLogger, // 여기서 mockLogger는 모킹된 Logger
+        },
+      ],
     }).compile();
 
     service = module.get<QuizWizardService>(QuizWizardService);
+    containerService = module.get<ContainersService>(ContainersService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('1번 문제 테스트 코드', () => {
+    let containerId: string;
+
+    beforeEach(async () => {
+      containerId = await containerService.getContainer(1);
+    });
+
+    afterEach(async () => {
+      await executeSSHCommand(`docker rm -f ${containerId}`);
+    });
+
+    it('init 이후 정답 받는 경우', async () => {
+      await executeSSHCommand(
+        `docker exec -w /home/quizzer/quiz -u quizzer ${containerId} git init`,
+      );
+
+      expect(await service.submit(containerId, 1)).toBe(true);
+    });
+
+    it('init 하지 않은 경우', async () => {
+      expect(await service.submit(containerId, 1)).toBe(false);
+    });
   });
 });
