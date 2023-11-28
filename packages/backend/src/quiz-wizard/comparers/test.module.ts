34353746183770e75e017c2fc0381ceed7a053db
flydog98 <guinnessoverflow@gmail.com>
2023-11-29 02:37:06 +0900

    refactor: 조금 더 정확한 함수명으로 변경
    [#145]

--- packages/backend/src/quiz-wizard/comparers/test.module.ts
+++ packages/backend/src/quiz-wizard/comparers/test.module.ts
@@ -22,7 +22,7 @@ export async function getConfig(
   return stdoutData;
 }
 
-export async function readGitIndex(container: string): Promise<string> {
+export async function getCachedDiff(container: string): Promise<string> {
   const { stdoutData } = await executeSSHCommand(
     `docker exec -u quizzer -w /home/quizzer/quiz ${container} git diff --cached`,
   );
