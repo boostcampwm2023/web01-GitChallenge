ad28e203b0f00df3c04a6588cfeb8ab7401687cf
flydog98 <guinnessoverflow@gmail.com>
2023-11-29 01:34:13 +0900

    feat: 3번 문제 채점 로직 구현
    [#145]

--- packages/backend/src/quiz-wizard/quiz-wizard.service.spec.ts
+++ packages/backend/src/quiz-wizard/quiz-wizard.service.spec.ts
@@ -86,4 +86,32 @@ describe('QuizWizardService', () => {
       expect(await service.submit(containerId, 2)).toBe(false);
     });
   });
+
+  describe('3번 문제 채점 테스트 코드', () => {
+    let containerId: string;
+
+    beforeEach(async () => {
+      containerId = await containerService.getContainer(3);
+    });
+
+    afterEach(async () => {
+      await executeSSHCommand(`docker rm -f ${containerId}`);
+    });
+
+    it('베스트 정답 케이스', async () => {
+      await executeSSHCommand(
+        `${DOCKER_TEMPLATE} ${containerId} git add README.md docs/plan.md`,
+      );
+
+      expect(await service.submit(containerId, 3)).toBe(true);
+    });
+
+    it('전부 다 add한 케이스', async () => {
+      await executeSSHCommand(
+        `${DOCKER_TEMPLATE} ${containerId} git add README.md docs/plan.md docs/architecture.md`,
+      );
+
+      expect(await service.submit(containerId, 3)).toBe(false);
+    });
+  });
 });
