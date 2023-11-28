44bad3c9eb1b261bdaedc03f19d1107b62a4ae5b
YuHyun <puh0128@gmail.com>
2023-11-28 14:51:20 +0900

    feat: Editor 명령모드 > 입력모드 변경 기능
    [#130]
    
    Co-Authored-By: CHAE <dbscogus4467@naver.com>

---
+++ packages/frontend/src/components/editor/Editor.tsx
@@ -0,0 +1,52 @@
+import { ChangeEventHandler, KeyboardEventHandler, useState } from "react";
+
+import * as styles from "./Editor.css";
+
+type ModeType = "insert" | "command";
+
+export function Editor() {
+  const [mode, setMode] = useState<ModeType>("command");
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [inputReadonly, setInputReadonly] = useState(true);
+  const [inputValue, setInputValue] = useState("");
+
+  const handleTextareaKeyDown: KeyboardEventHandler = (event) => {
+    const { key } = event;
+
+    if (isCommandMode(mode)) {
+      if (key === "i") {
+        setMode("insert");
+        setInputValue("-- INSERT --");
+        event.preventDefault();
+      }
+    }
+  };
+
+  const handleInputChange: ChangeEventHandler<HTMLInputElement> = ({
+    target: { value },
+  }) => {
+    setInputValue(value);
+  };
+
+  return (
+    <>
+      <span>{isCommandMode(mode) ? "명령모드" : "입력모드"}</span>
+      <div className={styles.container}>
+        <textarea
+          className={styles.textarea}
+          onKeyDown={handleTextareaKeyDown}
+        />
+        <input
+          className={styles.input}
+          type="text"
+          value={inputValue}
+          onChange={handleInputChange}
+        />
+      </div>
+    </>
+  );
+}
+
+function isCommandMode(mode: ModeType) {
+  return mode === "command";
+}
