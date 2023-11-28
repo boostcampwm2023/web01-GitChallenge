1cf1a791d422188e5d31a1ad652109a74bd2a9bd
CHAE <dbscogus4467@naver.com>
2023-11-28 19:01:52 +0900

    config: @testing-library/user-event, @testing-library/dom 설치, 세팅
    [#130]
    
    Co-Authored-By: 박유현 <puh0128@gmail.com>

--- packages/frontend/jest.config.mjs
+++ packages/frontend/jest.config.mjs
@@ -11,7 +11,7 @@ const config = {
     // Add more setup options before each test is run
     // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
 
-    testEnvironment: 'jest-environment-jsdom',
+    testEnvironment: 'jsdom',
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
