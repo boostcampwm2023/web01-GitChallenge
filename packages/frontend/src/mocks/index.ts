4e7ec783ca26cc697c74eea9208c05277a8f0b33
YuHyun <puh0128@gmail.com>
2023-11-28 09:56:57 +0900

    config: msw 초기 설정
    [#131]

---
+++ packages/frontend/src/mocks/index.ts
@@ -0,0 +1,17 @@
+async function initMocks() {
+  if (typeof window === "undefined") {
+    const { server } = await import("./server");
+    server.listen({
+      onUnhandledRequest: "bypass",
+    });
+  } else {
+    const { worker } = await import("./browser");
+    worker.start({
+      onUnhandledRequest: "bypass",
+    });
+  }
+}
+
+initMocks();
+
+export {};
