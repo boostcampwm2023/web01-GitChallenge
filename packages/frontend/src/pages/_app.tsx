2f8f21881cc244dd5d504b32eddbb6c51f402ff3
YuHyun <puh0128@gmail.com>
2023-11-28 09:56:57 +0900

    config: msw 초기 설정
    [#131]

--- packages/frontend/src/pages/_app.tsx
+++ packages/frontend/src/pages/_app.tsx
@@ -8,6 +8,10 @@ import "react-toastify/dist/ReactToastify.min.css";
 import { ToastContainer } from "../design-system/components/common";
 import Layout from "../design-system/components/common/Layout";
 
+if (process.env.NEXT_PUBLIC_API_MOCKING === "enabled") {
+  import("../mocks");
+}
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
