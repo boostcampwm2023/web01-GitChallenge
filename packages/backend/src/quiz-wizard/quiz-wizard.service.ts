cbbf7bc3c7c18bb7aabc870757bbdb40de6fb2c2
flydog98 <guinnessoverflow@gmail.com>
2023-11-28 23:52:57 +0900

    refactor: 미사용 함수 및 console.log 제거
    [#145]

--- packages/backend/src/quiz-wizard/quiz-wizard.service.ts
+++ packages/backend/src/quiz-wizard/quiz-wizard.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import fs from 'fs';
 import * as path from 'path';
 import { promisify } from 'util';
 import { exec } from 'child_process';
@@ -9,7 +8,6 @@ export class QuizWizardService {
   async submit(containerId, quizId) {
     const execAsync = promisify(exec);
 
-    console.log('현재 디렉토리 위치:', process.cwd());
     try {
       await execAsync(
         `yarn run jest ${path.resolve(
@@ -24,13 +22,5 @@ export class QuizWizardService {
     } catch (e) {
       return false;
     }
-  }
-  checkDirectoryExists(directoryPath) {
-    const doesExist = fs.existsSync(directoryPath);
-    describe('Directory Existence Check', () => {
-      it('should check if a directory exists', () => {
-        expect(doesExist).toBe(true);
-      });
-    });
   }
 }
