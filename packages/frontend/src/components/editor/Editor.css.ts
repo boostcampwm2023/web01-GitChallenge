241e427315ba73987eccca8aaad4d6f9aa905911
CHAE <dbscogus4467@naver.com>
2023-11-29 19:13:26 +0900

    style: 편집기 안내문 캡션 스타일링
    [#130]
    
    Co-Authored-By: 박유현 <puh0128@gmail.com>

--- packages/frontend/src/components/editor/Editor.css.ts
+++ packages/frontend/src/components/editor/Editor.css.ts
@@ -1,4 +1,13 @@
-import { style } from "@vanilla-extract/css";
+import { globalStyle, style } from "@vanilla-extract/css";
+
+import color from "../../design-system/tokens/color";
+import typography from "../../design-system/tokens/typography";
+import {
+  flexAlignCenter,
+  flexColumn,
+  flexJustifyCenter,
+  widthFull,
+} from "../../design-system/tokens/utils.css";
 
 export const container = style({
   display: "flex",
@@ -12,3 +21,31 @@ export const textarea = style({
 });
 
 export const input = style({});
+
+export const notice = style([
+  flexColumn,
+  flexJustifyCenter,
+  widthFull,
+  typography.$semantic.caption2Regular,
+  {
+    backgroundColor: color.$semantic.secondaryLow,
+    borderRadius: 8,
+    marginTop: 14,
+    gap: 5,
+    padding: 14,
+    color: color.$semantic.secondary,
+    whiteSpace: "break-spaces",
+    lineHeight: "165%",
+  },
+]);
+
+export const noticeItem = style([
+  flexAlignCenter,
+  {
+    gap: 5,
+  },
+]);
+
+globalStyle(`${noticeItem} > svg`, {
+  fontSize: 14,
+});
