43ec32d458526d8d51772e950b610198c5bc7876
flydog98 <guinnessoverflow@gmail.com>
2023-11-30 02:27:21 +0900

    test: 기본적인 e2e 환경 선언
    - 유닛 테스트에서 SSH 접속하는 그림이 좀 안 좋은 거 같아서 e2e로 해볼까 시도 중입니다.
    
    [#149]

--- packages/backend/test/app.e2e-spec.ts
+++ packages/backend/test/app.e2e-spec.ts
@@ -1,9 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import request from 'supertest';
+import { AppModule } from '../src/app.module';
 
-describe('AppController (e2e)', () => {
+describe('QuizWizardController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
@@ -15,10 +15,25 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  it('should solve quiz for git init command', async () => {
+    // 명령 실행
+    await request(app.getHttpServer())
+      .post('/api/v1/quizzes/1/command')
+      .send({
+        mode: 'command',
+        message: 'git init',
+      })
+      .expect(200);
+
+    // 퀴즈 제출 및 검증
+    const response = await request(app.getHttpServer())
+      .post('/api/v1/quizzes/1/submit')
+      .expect(200);
+
+    expect(response.body).toHaveProperty('solved', true);
+  });
+
+  afterEach(async () => {
+    await app.close();
   });
 });
