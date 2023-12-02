275f7ba5513b9c0793f4ec831a24a506674d23ee
CHAE <dbscogus4467@naver.com>
2023-12-02 16:16:51 +0900

    config: pages 폴더에 .css.ts 파일 추가하기 위한 extension 추가
    [#206]
    
    Co-Authored-By: 박유현 <puh0128@gmail.com>

--- packages/frontend/next.config.js
+++ packages/frontend/next.config.js
@@ -5,6 +5,7 @@ const withVanillaExtract = createVanillaExtractPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false, // react-toastify toast 두 번 렌더링되는 문제
+  pageExtensions: ["page.tsx", "page.ts", "page.jsx", "page.js"],
   async rewrites() {
     return [
       {
