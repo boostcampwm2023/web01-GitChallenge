5b05814d69069b24c65b29cd0d1d2bcabfbc12e8
YuHyun <puh0128@gmail.com>
2023-12-03 15:43:50 +0900

    feat: 편집기 안내 문구 수정
    [#133]

--- packages/frontend/src/components/editor/EditorInfo.tsx
+++ packages/frontend/src/components/editor/EditorInfo.tsx
@@ -5,12 +5,7 @@ import * as styles from "./Editor.css";
 export default function EditorInfo() {
   return (
     <div className={styles.notice}>
-      {[
-        `명령 모드에서 입력 모드로 전환하려면 "i"를 눌러주세요.`,
-        `입력 모드에서 마지막 라인 모드로 전환하려면 ":"을 눌러주세요.`,
-        `마지막 라인 모드와 입력 모드에서 명령모드로 전환하려면 "ESC"를 눌러주세요.`,
-        `마지막 라인 모드에서는 "q", "q!", "wq", "wq!" 명령어만 지원합니다.`,
-      ].map((line) => (
+      {NOTICE.map((line) => (
         <div key={line} className={styles.noticeItem}>
           <FaInfoCircle />
           {line}
@@ -19,3 +14,11 @@ export default function EditorInfo() {
     </div>
   );
 }
+
+const NOTICE = [
+  '텍스트를 작성하거나 수정하려면 "i"를 눌러 입력 모드로 전환해 주세요.',
+  '입력 모드에서 입력한 텍스트를 저장하거나 종료하려면 "ESC"와 ":"를 눌러 마지막 라인 모드로 전환해 주세요.',
+  `마지막 라인 모드에서 텍스트를 저장하지 않고 종료하려면 "q"와 "Enter"를 눌러주세요.`,
+  `마지막 라인 모드에서 텍스트를 저장하고 종료하려면 "wq"와 "Enter"를 눌러주세요.`,
+  '마지막 라인 모드에서 다시 입력 모드로 전환하고 싶다면 "ESC"와 "i"를 눌러주세요.',
+];
