bd289e38d4a4f33002a56b17f85607768ff9724b
CHAE <dbscogus4467@naver.com>
2023-12-04 18:21:20 +0900

    feat: 모범 답안 확인하기 모달창 구현
    [#8]

---
+++ packages/frontend/src/components/quiz/QuizAnswerModal/QuizAnswerModal.css.ts
@@ -0,0 +1,35 @@
+import { globalStyle, style } from "@vanilla-extract/css";
+
+import color from "../../../design-system/tokens/color";
+import typography from "../../../design-system/tokens/typography";
+import { noticeItem } from "../../editor/Editor.css";
+
+export const title = style([
+  typography.$semantic.h3,
+  { color: color.$scale.grey700, marginBottom: 20 },
+]);
+
+export const answerContainer = style([
+  typography.$semantic.body2Regular,
+  {
+    backgroundColor: color.$scale.grey50,
+    borderRadius: 8,
+    width: "100%",
+    padding: "23px 25px",
+    marginBottom: 19,
+    whiteSpace: "break-spaces",
+    color: color.$scale.grey700,
+  },
+]);
+
+export const answerNotice = style([
+  noticeItem,
+  typography.$semantic.caption1Regular,
+  {
+    color: color.$scale.grey600,
+  },
+]);
+
+globalStyle(`${answerContainer} code`, {
+  color: color.$scale.coral500,
+});
--- packages/frontend/src/components/quiz/QuizGuide/QuizGuide.tsx
+++ packages/frontend/src/components/quiz/QuizGuide/QuizGuide.tsx
@@ -1,22 +1,35 @@
 import { Button } from "../../../design-system/components/common";
+import useModal from "../../../hooks/useModal";
 import { Quiz } from "../../../types/quiz";
 import CommandAccordion from "../CommandAccordion";
+import QuizAnswerModal from "../QuizAnswerModal/QuizAnswerModal";
 import QuizContent from "../QuizContent";
 
 import * as styles from "./QuizGuide.css";
 
 export function QuizGuide({ quiz }: { quiz: Quiz }) {
+  const { modalOpen, openModal, closeModal } = useModal();
+
   return (
-    <div className={styles.quizContentContainer}>
-      <QuizContent
-        category={quiz.category}
-        title={quiz.title}
-        description={quiz.description}
-      />
-      <CommandAccordion width="100%" items={quiz.keywords} />
-      <Button className={styles.checkAnswerButton} variant="secondaryFill">
-        모범 답안 확인하기
-      </Button>
-    </div>
+    <>
+      {modalOpen && (
+        <QuizAnswerModal answer={quiz.answer} closeModal={closeModal} />
+      )}
+      <div className={styles.quizContentContainer}>
+        <QuizContent
+          category={quiz.category}
+          title={quiz.title}
+          description={quiz.description}
+        />
+        <CommandAccordion width="100%" items={quiz.keywords} />
+        <Button
+          className={styles.checkAnswerButton}
+          variant="secondaryFill"
+          onClick={openModal}
+        >
+          모범 답안 확인하기
+        </Button>
+      </div>
+    </>
   );
 }
