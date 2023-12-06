c0f428664272ee3d5995c9f19ca49cc3d33eb6b2
YuHyun <puh0128@gmail.com>
2023-12-07 01:45:16 +0900

    refactor: QuizAnswerModal 내 AnswerCodeBlock 컴포넌트 분리
    [#221]

---
+++ packages/frontend/src/components/quiz/AnswerCodeBlock/index.ts
@@ -0,0 +1,1 @@
+export { AnswerCodeBlock } from "./AnswerCodeBlock";
--- packages/frontend/src/components/quiz/QuizAnswerModal/QuizAnswerModal.tsx
+++ packages/frontend/src/components/quiz/QuizAnswerModal/QuizAnswerModal.tsx
@@ -1,5 +1,5 @@
 import { Info, Modal } from "../../../design-system/components/common";
-import { toCodeTag } from "../../../utils/mapper";
+import { AnswerCodeBlock } from "../AnswerCodeBlock";
 
 import * as styles from "./QuizAnswerModal.css";
 
@@ -15,12 +15,7 @@ export default function QuizAnswerModal({
   return (
     <Modal onClose={closeModal}>
       <h3 className={styles.h3}>모범 답안</h3>
-      <p
-        className={styles.answerContainer}
-        dangerouslySetInnerHTML={{
-          __html: toCodeTag(answer.join("\n")),
-        }}
-      />
+      <AnswerCodeBlock answer={answer} />
       <Info>
         본 답안은 모범 답안으로 제공되었으며, 상황에 따라 다양한 해결책이 있을
         수 있습니다.
