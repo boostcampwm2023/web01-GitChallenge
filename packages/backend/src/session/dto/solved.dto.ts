99c8c712ae3df79a1e80f9a5a3d25ab550396be4
luizy <luizy991212@gmail.com>
2023-12-06 14:21:33 +0900

    refactor: 퀴즈 개수 상수화
    [#244]

--- packages/backend/src/session/dto/solved.dto.ts
+++ packages/backend/src/session/dto/solved.dto.ts
@@ -1,8 +1,9 @@
+const QUIZ_COUNT = 19;
 export class SolvedDto {
   [key: number]: boolean;
 
   constructor() {
-    for (let i = 1; i <= 19; i++) {
+    for (let i = 1; i <= QUIZ_COUNT; i++) {
       this[i] = false;
     }
   }
