d7554e2c44ae4edf38566bd6906d6cc0f9c00fb5
luizy <luizy991212@gmail.com>
2023-12-06 22:08:01 +0900

    refactor: 응답 예시 수정
    [#16]

--- packages/backend/src/quizzes/dto/command-response.dto.ts
+++ packages/backend/src/quizzes/dto/command-response.dto.ts
@@ -24,16 +24,14 @@ export class CommandResponseDto {
   @ApiProperty({
     description: 'git 그래프 상황',
     example:
-      '{\n' +
-      '    "graph": [\n' +
-      '        {\n' +
-      '            "id": "0b6bb091c739e7aec2cb724378d50e486a914768",\n' +
-      '            "parentId": "",\n' +
-      '            "message": "docs: plan.md",\n' +
-      '            "refs": "HEAD -> main"\n' +
-      '        }\n' +
-      '    ]\n' +
-      '}',
+      '[\n' +
+      '    {\n' +
+      '        "id": "0b6bb091c739e7aec2cb724378d50e486a914768",\n' +
+      '        "parentId": "",\n' +
+      '        "message": "docs: plan.md",\n' +
+      '        "refs": "HEAD -> main"\n' +
+      '    }\n' +
+      ']\n',
   })
   graph?: object[];
 }
