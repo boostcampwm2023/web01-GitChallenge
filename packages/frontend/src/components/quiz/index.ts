bd7101080f878fad3aa0a3c4a56cf47bb23b09a7
CHAE <dbscogus4467@naver.com>
2023-12-06 19:09:58 +0900

    refactor: QuizAnswerModal export
    [#8]

--- packages/frontend/src/components/quiz/index.ts
+++ packages/frontend/src/components/quiz/index.ts
@@ -2,3 +2,4 @@ export { default as QuizContent } from "./QuizContent";
 export { default as QuizLocation } from "./QuizLocation";
 export { default as CommandAccordion } from "./CommandAccordion";
 export { SolvedModal, useSolvedModal } from "./SolvedModal";
+export { default as QuizAnswerModal } from "./QuizAnswerModal";
