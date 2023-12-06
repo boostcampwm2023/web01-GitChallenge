e50603f95fe56f045e7bbfcbd04b4ab3b75177c5
luizy <luizy991212@gmail.com>
2023-12-06 13:36:29 +0900

    feat: 풀이상태 제공 API
    [#244]

--- packages/backend/src/session/session.controller.ts
+++ packages/backend/src/session/session.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Delete, Res, UseInterceptors } from '@nestjs/common';
+import { Controller, Delete, Get, Res, UseInterceptors } from '@nestjs/common';
 import { SessionService } from './session.service';
 import { SessionId } from './session.decorator';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { Response } from 'express';
 import { SessionUpdateInterceptor } from './session-save.intercepter';
+import { SolvedDto } from './dto/solved.dto';
 
 @ApiTags('session')
 @Controller('api/v1/session')
@@ -30,5 +31,20 @@ export class SessionController {
     this.sessionService.deleteSession(sessionId);
     response.end();
     return;
+  }
+
+  @Get('/solved')
+  @ApiOperation({ summary: '해결한 문제들을 알려줍니다' })
+  @ApiResponse({
+    status: 200,
+    description: '해결한 문제들을 알려줍니다',
+    type: SolvedDto,
+  })
+  async getSolvedProblems(@SessionId() sessionId: string): Promise<SolvedDto> {
+    if (!sessionId) {
+      return new SolvedDto();
+    }
+
+    return await this.sessionService.getSolvedProblems(sessionId);
   }
 }
