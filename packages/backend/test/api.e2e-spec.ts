6e396996557cbf1a970f3991b20cdc6a38aeefac
flydog98 <guinnessoverflow@gmail.com>
2023-12-11 17:44:07 +0900

    test: ref 동작 테스트 코드 추가
    [#287]

--- packages/backend/test/api.e2e-spec.ts
+++ packages/backend/test/api.e2e-spec.ts
@@ -45,6 +45,112 @@ describe('QuizWizardController (e2e)', () => {
     });
   });
 
+  describe('ref 동작 테스트', () => {
+    let cookie: string;
+
+    it('최초 요청: 빈 문자열', async () => {
+      response = await request(app.getHttpServer()).get(
+        `/api/v1/quizzes/1/graph`,
+      );
+
+      expect(response.body.ref).toEqual('');
+    });
+
+    it('최초 요청: 브랜치', async () => {
+      response = await request(app.getHttpServer()).get(
+        `/api/v1/quizzes/9/graph`,
+      );
+
+      expect(response.body.ref).toEqual('feat/somethingB');
+    });
+
+    it('빈 문자열', async () => {
+      response = await request(app.getHttpServer())
+        .post(`/api/v1/quizzes/1/command`)
+        .send({
+          mode: 'command',
+          message: 'git status',
+        });
+
+      expect(response.body.ref).toEqual('');
+
+      cookie = response.headers['set-cookie'][0].split(';')[0];
+
+      await request(app.getHttpServer())
+        .delete(`/api/v1/quizzes/1/command`)
+        .set('Cookie', cookie);
+    });
+
+    it('브랜치명', async () => {
+      response = await request(app.getHttpServer())
+        .post(`/api/v1/quizzes/4/command`)
+        .send({
+          mode: 'command',
+          message: 'git status',
+        });
+
+      expect(response.body.ref).toEqual('main');
+
+      cookie = response.headers['set-cookie'][0].split(';')[0];
+
+      await request(app.getHttpServer())
+        .delete(`/api/v1/quizzes/4/command`)
+        .set('Cookie', cookie);
+    });
+
+    it('커밋 해시', async () => {
+      response = await request(app.getHttpServer())
+        .post(`/api/v1/quizzes/9/command`)
+        .send({
+          mode: 'command',
+          message: 'git checkout 55f7aab2f31287f6bb159731e5824566c02b582b',
+        });
+
+      expect(response.body.ref).toEqual('55f7aab');
+
+      cookie = response.headers['set-cookie'][0].split(';')[0];
+
+      await request(app.getHttpServer())
+        .delete(`/api/v1/quizzes/9/command`)
+        .set('Cookie', cookie);
+    });
+
+    it('editor 명령', async () => {
+      response = await request(app.getHttpServer())
+        .post(`/api/v1/quizzes/9/command`)
+        .send({
+          mode: 'command',
+          message: 'git add .',
+        });
+
+      expect(response.body.ref).toEqual('feat/somethingB');
+
+      cookie = response.headers['set-cookie'][0].split(';')[0];
+
+      response = await request(app.getHttpServer())
+        .post(`/api/v1/quizzes/9/command`)
+        .set('Cookie', cookie)
+        .send({
+          mode: 'command',
+          message: 'git commit',
+        });
+
+      response = await request(app.getHttpServer())
+        .post(`/api/v1/quizzes/9/command`)
+        .set('Cookie', cookie)
+        .send({
+          mode: 'editor',
+          message: 'test',
+        });
+
+      expect(response.body.ref).toEqual('feat/somethingB');
+
+      await request(app.getHttpServer())
+        .delete(`/api/v1/quizzes/9/command`)
+        .set('Cookie', cookie);
+    });
+  });
+
   afterAll(async () => {
     await app.close();
   });
