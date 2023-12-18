95b88cfeba28942ae10202e7ec67080ffebab359
flydog98 <guinnessoverflow@gmail.com>
2023-12-19 00:30:53 +0900

    feat: 컨테이너 서버 준비까지 기다리는 로직 추가
    [#300]

--- packages/backend/src/app.controller.ts
+++ packages/backend/src/app.controller.ts
@@ -1,12 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ContainersService } from './containers/containers.service';
 
-@Controller()
+@Controller('api/v1')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly containersService: ContainersService,
+  ) {}
 
-  // @Get()
-  // getHello(): string {
-  //   return this.appService.getHello();
-  // }
+  @Get('/health')
+  checkHealth(): string {
+    return this.containersService.isInitialized() ? 'OK' : 'Initializing';
+  }
 }
