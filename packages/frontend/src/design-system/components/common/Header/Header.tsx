14c8602d6727aa455aab43c44cc345add2c68f4a
YuHyun <puh0128@gmail.com>
2023-12-29 17:12:59 +0900

    feat: 헤더 > 퀴즈 페이지 튜토리얼 시작 버튼 추가
    [#304]

--- packages/frontend/src/design-system/components/common/Header/Header.tsx
+++ packages/frontend/src/design-system/components/common/Header/Header.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
+import { BROWSWER_PATH } from "../../../../constants/path";
+import { useQuizCoachActionContext } from "../../../../contexts/QuizCoachContext";
 import { ColorTheme } from "../../../../hooks/useTheme";
 import classnames from "../../../../utils/classnames";
 import { header as headerLayout } from "../../../tokens/layout.css";
@@ -12,6 +15,10 @@ import * as styles from "./Header.css";
 export default function Header() {
   const headerStyle = classnames(styles.borderBottom, headerLayout);
   const { colorTheme } = useThemeContext();
+
+  const router = useRouter();
+  const { handleStart } = useQuizCoachActionContext();
+  const quizPage = router.pathname.startsWith(`${BROWSWER_PATH.QUIZZES}/`);
 
   const imgMap = {
     light: "/light-logo.svg",
@@ -26,7 +33,18 @@ export default function Header() {
             <Image src={imgMap} alt="Git Challenge" width={238} height={30} />
           </Link>
         </h1>
-        <ThemeSelect />
+        <div className={styles.actionGroup}>
+          {quizPage && (
+            <button
+              type="button"
+              onClick={handleStart}
+              className={styles.tutorialButton}
+            >
+              튜토리얼
+            </button>
+          )}
+          <ThemeSelect />
+        </div>
       </div>
     </header>
   );
