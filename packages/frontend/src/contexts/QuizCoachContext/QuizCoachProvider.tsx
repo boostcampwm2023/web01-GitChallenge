fe0c1456bc7be2ff34c636ac12b7b22db12da76b
YuHyun <puh0128@gmail.com>
2023-12-30 00:09:20 +0900

    feat: 퀴즈 튜토리얼 완료 여부 값 로컬 스토리지로 관리
    [#304]

--- packages/frontend/src/contexts/QuizCoachContext/QuizCoachProvider.tsx
+++ packages/frontend/src/contexts/QuizCoachContext/QuizCoachProvider.tsx
@@ -1,6 +1,10 @@
-import { ReactNode, useCallback, useMemo, useState } from "react";
+import { ReactNode, useCallback, useEffect, useMemo, useState } from "react";
+
+import { createLocalStorage } from "../../utils/webStorage";
 
 import { QuizCoachActionContext, QuizCoachContext } from "./quizCoachContext";
+
+const quizTutorialEndStorage = createLocalStorage("quiz-tutorial-end", false);
 
 interface QuizCoachProviderProps {
   children: ReactNode;
@@ -11,16 +15,23 @@ export function QuizCoachProvider({ children }: QuizCoachProviderProps) {
 
   const handleEnd = useCallback(() => {
     setRun(false);
+    quizTutorialEndStorage.setItem(true);
   }, []);
 
   const handleStart = useCallback(() => {
     setRun(true);
+    quizTutorialEndStorage.removeItem();
   }, []);
 
   const quizCoachActionContextValue = useMemo(
     () => ({ handleEnd, handleStart }),
     [handleEnd, handleStart],
   );
+
+  useEffect(() => {
+    const tutorialEnd = quizTutorialEndStorage.getItem();
+    setRun(!tutorialEnd);
+  }, []);
 
   return (
     <QuizCoachContext.Provider value={run}>
