28230590052a81baa643961558c9c2f098ad0a34
CHAE <dbscogus4467@naver.com>
2024-01-18 15:35:14 +0900

    feat: 세션 초기화 API 추가
    [#13]

--- packages/frontend/src/apis/session.ts
+++ packages/frontend/src/apis/session.ts
@@ -4,6 +4,10 @@ import { UserQuizStatus } from "../types/user";
 import { instance } from "./base";
 
 export const sessionAPI = {
+  resetSession: async () => {
+    const { data } = await instance.delete(API_PATH.SESSION);
+    return data;
+  },
   getUserQuizStatus: async () => {
     const { data } = await instance.get<UserQuizStatus>(
       `${API_PATH.SESSION}/solved`,
